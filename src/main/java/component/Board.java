a1d0105021581ee2f876564df7d20f5822707607
ngawang30 <ngawang30@gmail.com>
2021-10-08 15:08:52 -0400

    V.01

---
+++ src/main/java/component/Board.java
@@ -0,0 +1,38 @@
+package component;
+
+public class Board {
+	boolean turn = true; //True == White's turn
+	/*
+	 * Uncomment once done with Piece classes.
+	 * Piece [][] currentBoard = new Piece[8][8];  
+	 */
+	
+	public Board() {
+		//For White and Black Pawn
+		for(int i = 0; i < 8; i++) {
+//			 currentBoard [6][i] = new Pawn()
+//			 currentBoard [1][i] = new Pawn()		 
+		};
+		
+		//Manual? 
+		//currentBoard[7][0] = new Rook();
+		//currentBoard[0][0] = new Rook();
+		//currentBoard[7][1] = new Knight();
+		//currentBoard[0][1] = new Knight();
+		//currentBoard[7][2] = new Bishop();
+		//currentBoard[0][2] = new Bishop();
+		//currentBoard[7][3] = new Queen();
+		//currentBoard[0][3] = new Queen();
+		//currentBoard[7][4] = new King();
+		//currentBoard[0][4] = new King();
+		//currentBoard[7][5] = new Bishop();
+		//currentBoard[0][5] = new Bishop();
+		//currentBoard[7][6] = new Knight();
+		//currentBoard[0][6] = new Knight();
+		//currentBoard[7][7] = new Rook();
+		//currentBoard[0][7] = new Rook();
+	}
+	
+	
+	
+}
